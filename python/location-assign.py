a60a7afee0b5b77b8f5fcd2814b759ca93c9a9b3
Nikko Gammad <nikko.gammad@gmail.com>
2026-02-10 16:10:01 +0800

    add basic gui python file. no actual gui yet.

---
+++ python/location-assign.py
@@ -0,0 +1,16 @@
+# ======
+# THIS PROGRAM COMPARES LOCATIONS IN A JSON FILE
+# AGAINST LOCATIONS IN THE PSGC
+# FOR LOCATION ASSIGNMENT.
+# AUTHOR: Nikko Gammad https://github.com/thirteenfour
+# ======
+
+# TKinter components
+from tkinter import *
+from tkinter import filedialog
+# dealing with csv
+import pandas as pd
+# JSON functions
+import json
+# for date and time
+from datetime import datetime
