568c814be6ad2237f87f1c2074dab33c7a3008f5
Nikko Gammad <nikko.gammad@gmail.com>
2026-02-11 15:37:52 +0800

    basic tkinter code added. not yet functional.

--- python/location-assign.py
+++ python/location-assign.py
@@ -14,3 +14,74 @@ import pandas as pd
 import json
 # for date and time
 from datetime import datetime
+
+# ======
+# TKinter
+# ======
+# create blank window
+window = Tk()
+# Set window title
+window.title('Location Assigner')
+# Set window size
+window.geometry("300x300")
+
+#####
+# Components
+#####
+# input label for locations with unknown assignment
+inputlabel = Label(window,
+                   text = "Input Locations JSON File:",
+                   width = 20,
+                   height = 3)
+# input user locations json file
+button_explore = Button(window, 
+                        text = "Browse Files",
+                        command = browseLocations,
+                        width = 15)
+# input label for input json
+label_json = Label(window,
+                   text = "Input PSGC JSON File:",
+                   width = 20,
+                   height = 3)
+# input json file
+button_explore_json = Button(window,
+                             text = "Browse Files",
+                             command = browseJSON,
+                             width = 15)
+# input month
+dropdown_month = OptionMenu(window, 
+                            selectedMonth, 
+                            *MONTHS)
+# input year
+text_year = Text(window, 
+                 height = 1,
+                 width = 10)
+# status label/debug label
+label_status = Label(window,
+                     text = STATUSSTRINGS[0],
+                     width = 20,
+                     height = 1)
+# process button
+button_process = Button(window,
+                        text="Process",
+                        command = processFile,
+                        width = 10)
+# exit button
+button_exit = Button(window, 
+                     text = "Exit",
+                     command = exit,
+                     width = 10) 
+
+# component layout
+label_json.grid(column = 1, row = 1)
+button_explore_json.grid(column = 2, row = 1)
+dropdown_month.grid(column = 1, row = 2)
+text_year.grid(column = 2, row = 2)
+inputlabel.grid(column = 1, row = 3)
+button_explore.grid(column = 2, row = 3)
+label_status.grid(column = 1, row = 4)
+button_process.grid(column = 2, row = 4)
+button_exit.grid(column = 1, row = 5)
+
+# enter event loop
+window.mainloop()
--- python/location-parser-gui.py
+++ python/location-parser-gui.py
@@ -115,3 +115,74 @@ print("total entries: " + str(total_entries))
 # dump to json
 filename_out_json = "out/philippines-location-data_" + current_year + current_quarter + "_" + time_now + ".json"
 json.dump(json_data, open(filename_out_json, "w", encoding="utf8"), indent=2, ensure_ascii=False)
+
+# ======
+# TKinter
+# ======
+# create blank window
+window = Tk()
+# Set window title
+window.title('PSGC Location Parser')
+# Set window size
+window.geometry("300x300")
+
+#####
+# Components
+#####
+# input label
+inputlabel = Label(window,
+                   text = "Input CSV File:",
+                   width = 20,
+                   height = 3)
+# input csv file
+button_explore = Button(window, 
+                        text = "Browse Files",
+                        command = browseCSV,
+                        width = 15)
+# input label for input json
+label_json = Label(window,
+                   text = "Input JSON File:",
+                   width = 20,
+                   height = 3)
+# input json file
+button_explore_json = Button(window,
+                             text = "Browse Files",
+                             command = browseJSON,
+                             width = 15)
+# input month
+dropdown_month = OptionMenu(window, 
+                            selectedMonth, 
+                            *MONTHS)
+# input year
+text_year = Text(window, 
+                 height = 1,
+                 width = 10)
+# status label/debug label
+label_status = Label(window,
+                     text = STATUSSTRINGS[0],
+                     width = 20,
+                     height = 1)
+# process button
+button_process = Button(window,
+                        text="Process",
+                        command = processFile,
+                        width = 10)
+# exit button
+button_exit = Button(window, 
+                     text = "Exit",
+                     command = exit,
+                     width = 10) 
+
+# component layout
+label_json.grid(column = 1, row = 1)
+button_explore_json.grid(column = 2, row = 1)
+dropdown_month.grid(column = 1, row = 2)
+text_year.grid(column = 2, row = 2)
+inputlabel.grid(column = 1, row = 3)
+button_explore.grid(column = 2, row = 3)
+label_status.grid(column = 1, row = 4)
+button_process.grid(column = 2, row = 4)
+button_exit.grid(column = 1, row = 5)
+
+# enter event loop
+window.mainloop()
